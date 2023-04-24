0746d03c31d698f6cfe1fe83748f9acbf74d0636
Fabian Ehrler <ehrler.fabian@gmail.com>
2023-04-24 16:16:40 +0200

    Add missing app ref. in models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 # app/models.py
 
 from datetime import datetime, timedelta
-from app import db, login
+from app import app, db, login
 from flask import url_for
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
